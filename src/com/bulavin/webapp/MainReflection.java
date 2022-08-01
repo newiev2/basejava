81bd9a7610af536adacd1de2f63b2bbe177a02c7
Nikita B <qopkawrka@gmail.com>
2022-08-01 11:57:06 +0300

    Lesson4. Added toString() method invocation via reflection

--- src/com/bulavin/webapp/MainReflection.java
+++ src/com/bulavin/webapp/MainReflection.java
@@ -3,17 +3,23 @@ package com.bulavin.webapp;
 import com.bulavin.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        String methodName = "toString";
+
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(resume));
         field.set(resume, "new_uuid");
-        // TODO invoke resume.toString() via reflection
-        System.out.println(resume);
+
+        // toString invocation via reflection
+        Method method = resume.getClass().getMethod(methodName);
+        System.out.println(method.invoke(resume));
     }
 }
