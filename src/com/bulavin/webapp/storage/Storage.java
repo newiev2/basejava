f01e05a5cec774f44cade9e845d03f4df304aadc
Nikita B <qopkawrka@gmail.com>
2022-08-22 14:45:01 +0300

    Lesson 6. Changed returned value to List<Resume> and renamed method to getAllSorted()

--- src/com/bulavin/webapp/storage/Storage.java
+++ src/com/bulavin/webapp/storage/Storage.java
@@ -2,6 +2,8 @@ package com.bulavin.webapp.storage;
 
 import com.bulavin.webapp.model.Resume;
 
+import java.util.List;
+
 public interface Storage {
 
     void save(Resume resume);
@@ -14,7 +16,7 @@ public interface Storage {
 
     void clear();
 
-    Resume[] getAll();
+    List<Resume> getAllSorted();
 
     int size();
 }
