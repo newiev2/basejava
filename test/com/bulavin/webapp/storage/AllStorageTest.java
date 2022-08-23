fd327e5c57a5f29f877ad23fc50053b6055d0aba
Nikita B <qopkawrka@gmail.com>
2022-08-23 09:30:37 +0300

    Lesson 6. Test for MapResumeStorage. Added it to suite class

--- test/com/bulavin/webapp/storage/AllStorageTest.java
+++ test/com/bulavin/webapp/storage/AllStorageTest.java
@@ -5,10 +5,11 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({
-      ArrayStorageTest.class,
-      SortedArrayStorageTest.class,
-      ListStorageTest.class,
-      MapUuidStorageTest.class
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+        MapResumeStorageTest.class
 })
 public class AllStorageTest {
 }
---
+++ test/com/bulavin/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,8 @@
+package com.bulavin.webapp.storage;
+
+class MapResumeStorageTest extends AbstractStorageTest {
+
+    protected MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
