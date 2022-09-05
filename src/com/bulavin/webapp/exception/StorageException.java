99df994c91051d8da380f32ba459a84b13d59641
Nikita B <qopkawrka@gmail.com>
2022-09-05 15:42:34 +0300

    Lesson 8. Added implementation for AbstractFileStorage. Added constructor to StorageException without uuid for file exceptions

--- src/com/bulavin/webapp/exception/StorageException.java
+++ src/com/bulavin/webapp/exception/StorageException.java
@@ -3,8 +3,18 @@ package com.bulavin.webapp.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+    public StorageException(String message) {
+        super(message);
+        uuid = null;
+    }
+
     public StorageException(String uuid, String message) {
         super(message);
+        this.uuid = uuid;
+    }
+
+    public StorageException(String uuid, String message, Exception cause) {
+        super(message, cause);
         this.uuid = uuid;
     }
 
---
+++ src/com/bulavin/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,107 @@
+package com.bulavin.webapp.storage;
+
+import com.bulavin.webapp.exception.StorageException;
+import com.bulavin.webapp.model.Resume;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "Directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected void saveResume(Resume resume, File searchKey) {
+        try {
+            searchKey.createNewFile();
+            writeData(resume, searchKey);
+        } catch (Exception e) {
+            throw new StorageException(searchKey.getName(), "Error occurred while trying to save resume", e);
+        }
+    }
+
+    @Override
+    protected Resume getResume(File searchKey) {
+        try {
+            return readData(searchKey);
+        } catch (Exception e) {
+            throw new StorageException(searchKey.getName(), "Error occurred while trying to get resume", e);
+        }
+    }
+
+    @Override
+    protected void updateResume(Resume resume, File searchKey) {
+        try {
+            writeData(resume, searchKey);
+        } catch (Exception e) {
+            throw new StorageException(searchKey.getName(), "Error occurred while trying to update resume", e);
+        }
+    }
+
+    @Override
+    protected void deleteResume(File searchKey) {
+        try {
+            searchKey.delete();
+        } catch (Exception e) {
+            throw new StorageException(searchKey.getName(), "Error occurred while trying to delete resume", e);
+        }
+    }
+
+    @Override
+    protected List<Resume> getAllElements() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            List<Resume> resumes = new ArrayList<>();
+            for (File file : files) {
+                resumes.add(readData(file));
+            }
+            return resumes;
+        } else throw new StorageException("Error occurred while trying to get all resumes");
+    }
+
+    @Override
+    protected boolean isExisting(File searchKey) {
+        return searchKey.exists();
+    }
+
+    @Override
+    protected File findSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    public void clear() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                deleteResume(file);
+            }
+        } else throw new StorageException("Directory is empty. Nothing to clear");
+    }
+
+    @Override
+    public int size() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            return files.length;
+        }
+        return 0;
+    }
+
+    protected abstract void writeData(Resume resume, File file);
+
+    protected abstract Resume readData(File file);
+}
